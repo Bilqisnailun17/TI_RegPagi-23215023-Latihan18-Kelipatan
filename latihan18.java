9fe2570d1d8f8306056dde8ddb64cb4236c8fcec
Bilqisnailun17 <bilqisnailunnajla@gmail.com>
2024-11-04 15:01:23 +0700

    Add files via upload

---
+++ latihan18.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 62818
+ */
+public class latihan18 {
+    public static void main(String[] args) {
+        double angka = 3.5;
+        
+        System.out.println("Output:");
+        
+      
+        while (angka <= 35) {
+            System.out.println(angka);
+            angka += 3.5;
+        }
+    }
+}
